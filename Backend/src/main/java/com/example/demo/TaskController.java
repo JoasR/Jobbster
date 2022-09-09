c36adecba323b125c8f3d539f59893dcf1b7dbf6
JoasR <joas.rothig@hotmail.com>
2022-09-09 18:55:35 +0200

    Added first version of Post Job

--- Backend/src/main/java/com/example/demo/TaskController.java
+++ Backend/src/main/java/com/example/demo/TaskController.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 @RequestMapping(path = "api/v1/task")
 public class TaskController {
@@ -16,7 +18,7 @@ public class TaskController {
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
-
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping
     public List<Task> getTasks(){
         return taskService.getTask();
---
+++ Frontend/app.js
@@ -0,0 +1,11 @@
+// const showHere = document.getElementById("showHere");
+
+// fetch('http://localhost:8080/api/v1/task')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     data.forEach( task => {
+//       const id = `<h3> ${task.id} </h3>`
+//       showHere.innerHTML += id;
+//     })  
+//     }
+//   );
