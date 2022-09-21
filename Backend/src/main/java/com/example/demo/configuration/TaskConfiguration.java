82fac2899f7d94daf22e6a96c9a3e36f9cce9596
JoasR <joas.rothig@hotmail.com>
2022-09-21 16:52:31 +0200

    Added extra items in the taskconfiguration

--- Backend/src/main/java/com/example/demo/configuration/TaskConfiguration.java
+++ Backend/src/main/java/com/example/demo/configuration/TaskConfiguration.java
@@ -36,7 +36,63 @@ public class TaskConfiguration {
                     "ThisIsAnImageUrl2",
                     LocalDateTime.now()
                     );
-            taskDAO.saveAll(List.of(task1, task2));
+            Task task3 = new Task(
+                    "Task 3 Title",
+                    "Task 3 Description",
+                    Category.ANIMALS,
+                    "ThisIsAnImageUrl3",
+                    LocalDateTime.now()
+            );
+            Task task4 = new Task(
+                    "Task 4 Title",
+                    "Task 4 Description",
+                    Category.GARDENING,
+                    "ThisIsAnImageUrl4",
+                    LocalDateTime.now()
+            );
+            Task task5 = new Task(
+                    "Task 5 Title",
+                    "Task 5 Description",
+                    Category.ANIMALS,
+                    "ThisIsAnImageUrl5",
+                    LocalDateTime.now()
+            );
+            Task task6 = new Task(
+                    "Task 6 Title",
+                    "Task 6 Description",
+                    Category.GARDENING,
+                    "ThisIsAnImageUrl6",
+                    LocalDateTime.now()
+            );
+            Task task7 = new Task(
+                    "Task 7 Title",
+                    "Task 7 Description",
+                    Category.ANIMALS,
+                    "ThisIsAnImageUrl7",
+                    LocalDateTime.now()
+            );
+            Task task8 = new Task(
+                    "Task 8 Title",
+                    "Task 8 Description",
+                    Category.GARDENING,
+                    "ThisIsAnImageUrl8",
+                    LocalDateTime.now()
+            );
+            Task task9 = new Task(
+                    "Task 9 Title",
+                    "Task 9 Description",
+                    Category.ANIMALS,
+                    "ThisIsAnImageUrl9",
+                    LocalDateTime.now()
+            );
+            Task task10 = new Task(
+                    "Task 10 Title",
+                    "Task 10 Description",
+                    Category.GARDENING,
+                    "ThisIsAnImageUrl10",
+                    LocalDateTime.now()
+            );
+            taskDAO.saveAll(List.of(task1, task2, task3, task4, task5, task6, task7, task8, task9, task10));
         };
     }
 }
